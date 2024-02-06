15ec552bade8e161e74efe136631175e9d356eb1
Bob Stanley <dakotasfacts@gmail.com>
2024-02-06 23:28:33 +0000

    Done with splashContent function -This function populates the body of the html page

---
+++ src/splash.js
@@ -0,0 +1,11 @@
+const splashContent = function() {
+    let content = `<div class="splash-container">
+                        <h1 class="title">
+                        Mono Weather
+                        </h1>
+                    </div>`
+
+    document.body.innerHTML = content;
+}
+
+export default splashContent;
